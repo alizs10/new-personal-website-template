065efb74407f158b181a21c027b0ac26b1088955
Ali Z.Soleimani <ali.text77@gmail.com>
2022-07-21 12:27:27 +0430

    add home page

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import './styles/fonts/manototv/manototvkalamefont.ttf';
 import './styles/fonts/manototv/manototvkalamefont.woff';
 import './styles/fonts/manototv/manototvkalamefont.woff2';
 import './styles/fonts/manototv/stylesheet.css';
+import './styles/fontawesome/css/all.min.css';
+import './js/all.min';
+import './assets/profile-photo.jpg';
+
